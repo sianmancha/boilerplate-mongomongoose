84d8d722cbc46a7513c78098a2cac4109ebde63f
Sian Mancha <slmancha20@gmail.com>
2022-03-31 17:07:02 -0500

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+require('mongoose').config();
 
+// 1. Install and Set Up Mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
